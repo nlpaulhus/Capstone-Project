601b54bfed00e4a50d24d2d9621a671d197c8730
Nicole Paulhus <nlpaulhus@gmail.com>
2025-10-04 12:50:43 -0700

    Carousel hover and click working

--- frontend/src/components/CreditBox/ProfileCreditBox.jsx
+++ frontend/src/components/CreditBox/ProfileCreditBox.jsx
@@ -1,11 +1,18 @@
-
 import Badge from "react-bootstrap/Badge";
 
 const ProfileCreditBox = ({ credit, inNetwork }) => {
+  function onClick(e) {}
   return (
-    <div className="profileCreditBox">
-      <img className="profileCreditImage" src={credit.image}></img>
-    </div>
+    <a href={`http://imdb.com/title/${credit.id}`} target="_blank">
+      <div className="profileCreditBox">
+        <img
+          id={credit.id}
+          className="profileCreditImage"
+          src={credit.image}
+        ></img>
+        <div className="overlay">{credit.title}</div>
+      </div>
+    </a>
   );
 };
 
