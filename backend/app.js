41756b1ed422ce6cdd12c517ca44c157348dc2ad
Nicole Paulhus <nlpaulhus@gmail.com>
2025-10-15 15:06:23 -0700

    without cors and written out

--- backend/app.js
+++ backend/app.js
@@ -11,14 +11,42 @@ const app = express();
 import session from "express-session";
 
 //Import & apply cors:
-import cors from "cors";
+// import cors from "cors";
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: /.*\.get-a-grip-rij7.onrender\.com$/,
+//     credentials: true,
+//   })
+// );
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://get-a-grip-rij7.onrender.com/"
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //Middleware:
 app.use(json());
